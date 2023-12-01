01456118e396cd5982efdea3d5d0c127b4f1e0c4
SoonHyuck <shtnsgur8@naver.com>
2023-12-01 14:21:29 +0900

    right hand test

---
+++ MediaPipe/Pose.py
@@ -0,0 +1,104 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+from time import time
+import math
+import matplotlib.pyplot as plt
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+def calculateAngle(landmark1, landmark2, landmark3):
+    x1, y1, _ = landmark1
+    x2, y2, _ = landmark2
+    x3, y3, _ = landmark3
+
+    angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
+    if angle <0 :
+        angle += 360
+    return angle
+
+def classifyPose(landmarks, output_image, display=False):
+    label = 'Unknown Pose'
+    color = (0,0,255)
+    # 11번, 13번, 15번 landmark 
+    # 왼쪽 어깨, 왼쪽 팔꿈치, 왼쪽 손목 landmark angle 값 계산 
+    left_elbow_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value],
+                                      landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value],
+                                      landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value])
+    
+    # 12번, 14번, 16번 landmark 
+    # 오른쪽 어깨, 오른쪽 팔꿈치, 오른쪽 손목 landmark angle 값 계산 
+    right_elbow_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value],
+                                       landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value],
+                                       landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value])   
+    
+    
+    if right_elbow_angle > 165 and  right_elbow_angle > 195:
+        label = "right hand shift"
+    
+    if left_elbow_angle > 165 and left_elbow_angle < 195 :
+        label = "left hand shift"
+    
+    if label != 'Unknown Pose':
+        color = (0, 255, 0)
+    
+    cv2.putText(output_image, label, (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
+    
+    return output_image, label
+
+    
+# 웹캠, 영상 파일의 경우 이것을 사용하세요.:
+cap = cv2.VideoCapture(0)
+
+with mp_pose.Pose(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as pose:
+
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("카메라를 찾을 수 없습니다.")
+            # 동영상을 불러올 경우는 'continue' 대신 'break'를 사용합니다.
+            continue
+
+        # 필요에 따라 성능 향상을 위해 이미지 작성을 불가능함으로 기본 설정합니다.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        results = pose.process(image) #전처리
+
+        keypoints = []
+        if results.pose_landmarks:
+            for data_point in results.pose_landmarks.landmark:
+                keypoints.append({
+                    'X': data_point.x,
+                    'Y': data_point.y,
+                    'Z': data_point.z,
+                    'Visibility': data_point.visibility,
+                })
+        
+        RIGHT_SHOULDER = results.pose_landmarks.landmark[12].x, results.pose_landmarks.landmark[12].y, results.pose_landmarks.landmark[12].z
+        RIGHT_ELBOW = results.pose_landmarks.landmark[14].x, results.pose_landmarks.landmark[14].y, results.pose_landmarks.landmark[14].z
+        RIGHT_WRIST = results.pose_landmarks.landmark[16].x, results.pose_landmarks.landmark[16].y, results.pose_landmarks.landmark[16].z
+        
+        angle = calculateAngle(RIGHT_SHOULDER, RIGHT_ELBOW, RIGHT_WRIST)
+        angle_text = f'계산된 각도의 값은 {angle:.2f}입니다.'
+        
+        # 포즈 주석을 이미지 위에 그립니다.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        mp_drawing.draw_landmarks(
+            image,
+            results.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        
+        cv2.putText(image, angle_text, org=(10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 255, 0),thickness=2)
+
+        # 보기 편하게 이미지를 좌우 반전합니다.
+        cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+        if cv2.waitKey(1) == ord('q'):
+            break
+cap.release()
---
+++ MediaPipe/ReadMe.txt
@@ -0,0 +1,11 @@
+import math
+import cv2
+import numpy as np
+from time import time
+import mediapipe as mp
+import matplotlib.pyplot as plt
+
+pip install opencv-python==4.5.5.64
+pip install mediapipe
+
+// 주피터 가상환경 기본셋팅
