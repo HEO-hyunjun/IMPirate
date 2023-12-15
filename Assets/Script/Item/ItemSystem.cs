15946e8724fede3e5d3e5779d88c8868f211aed0
Haein <bloom7gloom@gmail.com>
2023-12-16 04:19:11 +0900

    대기실 삭제, 게임신과 병합

--- Assets/Script/Item/ItemSystem.cs
+++ Assets/Script/Item/ItemSystem.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 [RequireComponent(typeof(PlayerStatSystem))]
 public class ItemSystem : MonoBehaviour
